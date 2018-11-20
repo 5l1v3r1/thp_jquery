849bc91e3c8efe166b51f5b70595b27126c41608
zOmegad <simonrichard1998@gmail.com>
2018-11-20 22:33:56 +0100

    debug heroku

--- app/assets/javascripts/channels/dropdown.js
+++ app/assets/javascripts/channels/dropdown.js
@@ -1,6 +1,4 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
+  function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
